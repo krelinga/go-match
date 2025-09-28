58f873f73c88f07b97bbb7b0085aaa71ab95b6a5
Andy Kreling <krelinga@gmail.com>
2025-09-28 07:27:42 +0000

    full tests for Equal().

--- comparable_test.go
+++ comparable_test.go
@@ -1,16 +1,52 @@
 package match_test
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/krelinga/go-match"
 )
 
 func TestEqual(t *testing.T) {
-	m := match.Equal[int]{X: 42}
-	if !match.Match(42, m) {
-		t.Error("Expected match")
+	goldie := newGoldie(t)
+	tests := []struct {
+		name    string
+		m match.Equal[string]
+		val string
+		want bool
+	}{
+		{
+			name: "match",
+			m:    match.Equal[string]{X: "hello"},
+			val:  "hello",
+			want: true,
+		},
+		{
+			name: "no_match",
+			m:    match.Equal[string]{X: "hello"},
+			val:  "world",
+			want: false,
+		},
+		{
+			name: "match_format",
+			m:    match.Equal[string]{X: "hello", Format: strings.ToUpper},
+			val:  "hello",
+			want: true,
+		},
+		{
+			name: "no_match_format",
+			m:    match.Equal[string]{X: "hello", Format: strings.ToUpper},
+			val:  "world",
+			want: false,
+		},
 	}
-	t.Logf("\n%s", match.Explain(42, m))
-	t.Logf("\n%s", match.Explain(43, m))
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := match.Match(tt.val, tt.m)
+			if got != tt.want {
+				t.Errorf("Match() = %v, want %v", got, tt.want)
+			}
+			goldie.Assert(t, tt.name, []byte(match.Explain(tt.val, tt.m)))
+		})
+	}
 }
